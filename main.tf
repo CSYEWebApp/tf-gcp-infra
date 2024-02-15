b36904cdb7196c571bffd68e241e39e105ab5352
Vamsidhar858 <145138793+Vamsidhar858@users.noreply.github.com>
2024-02-15 15:12:47 -0500

    Removed Dead Code

--- main.tf
+++ main.tf
@@ -1,37 +1,3 @@
-#provider "google" {
-#  project = var.project_id
-#  region  = var.region
-#}
-#resource "google_compute_network" "vpc_network" {
-#  name                            = var.vpc_name
-#  delete_default_routes_on_create = true
-#  auto_create_subnetworks         = false
-#  routing_mode                    = "REGIONAL"
-#}
-#resource "google_compute_global_address" "internet_gateway_ip" {
-#  name = var.internet_gateway_name
-#}
-#
-#resource "google_compute_subnetwork" "webapp_subnet" {
-#  name                      = "${var.vpc_name}-webapp-subnet"
-#  ip_cidr_range             = var.webapp_subnet_cidr
-#  region                    = var.region
-#  network                   = google_compute_network.vpc_network.self_link
-#}
-#resource "google_compute_subnetwork" "db_subnet" {
-#  name                      = "${var.vpc_name}-db-subnet"
-#  ip_cidr_range             = var.db_subnet_cidr
-#  region                    = var.region
-#  network                   = google_compute_network.vpc_network.self_link
-#}
-#resource "google_compute_route" "default_route" {
-#  name                  = "default-route"
-#  network               = google_compute_network.vpc_network.self_link
-#  dest_range            = "0.0.0.0/0"
-#  next_hop_ip           = google_compute_global_address.internet_gateway_ip.address
-#  priority              = 1000
-#}
-
 provider "google" {
   project = var.project_id
   region  = var.region
--- variables.tf
+++ variables.tf
@@ -1,39 +1,3 @@
-#terraform {
-#  required_providers {
-#    google = {
-#      source  = "hashicorp/google"
-#      version = "~> 4.0"
-#    }
-#  }
-#}
-#
-#variable "project_id" {
-#  description = "The ID of your GCP project"
-#}
-#
-#variable "region" {
-#  description = "The region where you want to create the VPC and subnets"
-#}
-#
-#variable "vpc_name" {
-#  description = "The name of your VPC network"
-#}
-#
-#variable "internet_gateway_name" {
-#  description = "The name of your internet gateway"
-#}
-#
-#variable "webapp_subnet_cidr" {
-#  description = "The CIDR range for the webapp subnet (/24 format)"
-#}
-#
-#variable "db_subnet_cidr" {
-#  description = "The CIDR range for the db subnet (/24 format)"
-#}
-#
-#
-#
-
 variable "project_id" {
   description = "The ID of the GCP project."
 }
